b66666e4de76bd943290641bc0c9c8adf8ee16ed
SebastianDev807 <sebastian.dev0708@gmail.com>
2025-05-26 18:10:08 -0500

    Añadiendo tabla hash

--- src/main/java/org/juego/logica/hash/RegistroJugador.java
+++ src/main/java/org/juego/logica/hash/RegistroJugador.java
@@ -1,5 +1,14 @@
 package org.juego.logica.hash;
 
+import org.juego.modelos.Carta;
+
+/**
+ * Registro de datos para cada participante en el juego.
+ * Almacena el seguimiento de las cartas que se han jugado, las que se han ganado,
+ * la energía reunida y las rondas que se han ganado.
+ * @author David Cabrera
+ *
+ */
 public class RegistroJugador {
     private int cartasJugadas;
     private int cartasGanadas;
@@ -11,5 +20,40 @@ public class RegistroJugador {
         this.cartasGanadas = 0;
         this.poderAcumulado = 0;
         this.rondasGanadas = 0;
+    }
+
+    public void registrarCartaJugada(Carta carta) {
+        cartasJugadas++;
+        poderAcumulado += carta.getPoder();
+    }
+
+    public void registrarVictoria(Carta carta) {
+        cartasGanadas++;
+        rondasGanadas++;
+        poderAcumulado += carta.getPoder();
+    }
+
+    public int getCartasJugadas() {
+        return cartasJugadas;
+    }
+
+    public int getCartasGanadas() {
+        return cartasGanadas;
+    }
+
+    public int getPoderAcumulado() {
+        return poderAcumulado;
+    }
+
+    public int getRondasGanadas() {
+        return rondasGanadas;
+    }
+
+    @Override
+    public String toString() {
+        return "Cartas jugadas: " + cartasJugadas +
+                ", Cartas ganadas: " + cartasGanadas +
+                ", Poder acumulado: " + poderAcumulado +
+                ", Rondas ganadas: " + rondasGanadas;
     }
 }
