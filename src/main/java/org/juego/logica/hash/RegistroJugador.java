c504311fb59d56db357f141cfe3f38ce88c24762
SebastianDev807 <sebastian.dev0708@gmail.com>
2025-05-26 17:51:08 -0500

    Añadiendo arbol binario

---
+++ src/main/java/org/juego/logica/hash/RegistroJugador.java
@@ -0,0 +1,15 @@
+package org.juego.logica.hash;
+
+public class RegistroJugador {
+    private int cartasJugadas;
+    private int cartasGanadas;
+    private int poderAcumulado;
+    private int rondasGanadas;
+
+    public RegistroJugador() {
+        this.cartasJugadas = 0;
+        this.cartasGanadas = 0;
+        this.poderAcumulado = 0;
+        this.rondasGanadas = 0;
+    }
+}
