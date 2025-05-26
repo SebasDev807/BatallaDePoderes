de92a686c6dbb1c3e0b9643ba391c1a65d9893d2
SebastianDev807 <sebastian.dev0708@gmail.com>
2025-05-26 12:50:01 -0500

    añadiendo Cola

---
+++ src/main/java/org/juego/estructuras/Cola.java
@@ -0,0 +1,83 @@
+package org.juego.estructuras;
+
+import org.juego.modelos.Carta;
+
+/**
+ * Estructura de cola que representa el mazo de un jugador
+ * Funciona en orden FIFO (First In - First Out)
+ *
+ * @author Juan Sebastián Astudillo Ordoñez
+ */
+public class Cola {
+
+    private NodoCola frente;
+    private NodoCola fin;
+    private int size;
+
+    public Cola() {
+        frente = null;
+        fin = null;
+        size = 0;
+    }
+
+    /**
+     * Inserta una carta al final de la cola.
+     *
+     * @param carta Carta a insertar
+     */
+
+    public void encolar(Carta carta) {
+        NodoCola nuevo = new NodoCola(carta);
+
+        if (estaVacia()) {
+            frente = nuevo;
+            fin = nuevo;
+        } else {
+            fin.setSiguiente(nuevo);
+            fin = nuevo;
+        }
+        size++;
+    }
+
+    /**
+     * @return Carta retirada, o null si la cola está vacía
+     */
+    public Carta desencolar() {
+        if (estaVacia()) return null;
+
+        Carta carta = frente.getCarta();
+        frente = frente.getSiguiente();
+
+        if (frente == null) fin = null;
+
+        size--;
+        return carta;
+
+    }
+
+    /**
+     *
+     * devuelve la carta al frente sin retirarla
+     */
+    public Carta verFrente(){
+        return estaVacia() ? null : frente.getCarta(); //Esto es un operador ternario '?' representa if ':' representa else
+    }
+
+
+    public boolean estaVacia() {
+        return frente == null;
+    }
+
+    public int getSize(){
+        return size;
+    }
+
+    public void mostrarCartas(){
+        NodoCola actual = frente;
+        while(actual != null){
+            System.out.println(actual.getCarta());
+            actual = actual.getSiguiente();
+        }
+    }
+
+}
